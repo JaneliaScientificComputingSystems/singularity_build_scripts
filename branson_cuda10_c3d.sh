847760dd633431d1ea4036d7d7c9c9a51f048dbe
enielson <nielsone10@nielsone10-ws2.int.janelia.org>
2019-05-03 14:11:18 -0400

    Added dudman_aigym buil scripts.

--- branson_cuda10_c3d.sh
+++ branson_cuda10_c3d.sh
@@ -1,6 +1,6 @@
 IMG="/singularity/images/branson_cuda10_c3d.img"
 SIMG="/singularity/images/branson_cuda10_c3d.simg"
-DEF="branson_cuda10.def"
+DEF="branson_cuda10_c3d.def"
 singularity build -s $IMG $DEF
 singularity exec -B /singularity/nvidia:/mnt -w $IMG sh /mnt/cuda_10.0.130_410.48_linux.run --toolkit --silent
 singularity exec -B /singularity/nvidia:/mnt -w $IMG tar xvzf /mnt/cudnn-10.0-linux-x64-v7.5.0.56.tgz -C /usr/local
---
+++ dudman_aigym.sh
@@ -0,0 +1,7 @@
+IMG="/singularity/images/dudman_aigym.img"
+SIMG="/singularity/images/dudman_aigym.simg"
+DEF="dudman_aigym.def"
+singularity build -s $IMG $DEF
+singularity exec -B /singularity/nvidia:/mnt -w $IMG sh /mnt/cuda_10.0.130_410.48_linux.run --toolkit --silent
+singularity exec -B /singularity/nvidia:/mnt -w $IMG tar xvzf /mnt/cudnn-10.0-linux-x64-v7.5.0.56.tgz -C /usr/local
+singularity build $SIMG $IMG
