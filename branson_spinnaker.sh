b6e07b3c4ea1292ba7d35e89ae5eb4a353164ac3
enielson <nielsone10@nielsone10-ws2.int.janelia.org>
2019-05-03 11:13:46 -0400

    added c3d image build scripts.

--- branson_spinnaker.sh
+++ branson_spinnaker.sh
@@ -5,4 +5,6 @@ singularity build -s $IMG $DEF
 singularity exec -B /singularity/nvidia:/mnt -w $IMG sh /mnt/cuda_9.0.176_384.81_linux.run -toolkit -silent
 singularity exec -B /singularity/nvidia:/mnt -w $IMG tar xvzf /mnt/cudnn-9.0-linux-x64-v7.tgz -C /usr/local
 singularity exec -B /singularity/spinnaker-1.13.0.31-amd64:/opt/spinnaker -w $IMG sh /opt/spinnaker/install_spinnaker.sh
+singularity exec -w /singularity/images/branson_spinnaker.img /opt/mntqt/qt-unified-linux-x64-3.1.0-online.run
+#sudo -u nielsone10 singularity exec -w $IMG export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb && /opt/mntqt/qt-unified-linux-x64-3.1.0-online.run
 singularity build $SIMG $IMG
