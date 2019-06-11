cd9c15c28a687d8ca49edeb9966fb145a3b694a5
enielson <root@nielsone10-ws2.int.janelia.org>
2019-06-11 10:54:16 -0400

    Added build scripts for Claudias image.

--- boehmc_keras.sh
+++ boehmc_keras.sh
@@ -1,6 +1,6 @@
-IMG="/singularity/images/branson_v4.img"
-SIMG="/singularity/images/branson_v4.simg"
-DEF="branson_v4.def"
+IMG="/singularity/images/boehmc_keras.img"
+SIMG="/singularity/images/boehmc_keras.simg"
+DEF="boehmc_keras.def"
 singularity build -s $IMG $DEF
 singularity exec -B /singularity/nvidia:/mnt -w $IMG sh /mnt/cuda_10.0.130_410.48_linux.run --toolkit --silent
 singularity exec -B /singularity/nvidia:/mnt -w $IMG tar xvzf /mnt/cudnn-10.0-linux-x64-v7.5.0.56.tgz -C /usr/local
